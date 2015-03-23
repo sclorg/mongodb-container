46bac5bd8688e6f367ed2d53d2185be7d841efe9
jhadvig <j.hadvig@gmail.com>
2015-03-23 16:40:03 +0100

    Adding various changes

--- 2.4/run-mongod.sh
+++ 2.4/run-mongod.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# SCL in CentOS/RHEL 7 doesn't support --exec, we need to do it ourselves
-source scl_source enable mongodb24
-set -e
+# For SCL enablement
+source /var/lib/mongodb/.bashrc
+
+set -eu
 
 MAX_ATTEMPTS=60
 SLEEP_TIME=1
@@ -19,37 +20,42 @@ function usage() {
 }
 
 function up_test() {
-    for i in $(seq $MAX_ATTEMPTS); do
-        echo "=> Waiting for confirmation of MongoDB service startup"
-        set +e
-        mongo admin --eval "help"
-        status=$?
-        set -e
-        if [ $status -eq 0 ]; then
-            echo "=> MongoDB service has started"
-            return 0
-        fi
-        sleep $SLEEP_TIME
-    done
-    echo "=> Giving up: Failed to start MongoDB service"
-    exit 1
-}
+	for i in $(seq $MAX_ATTEMPTS); do
+		echo "=> Waiting for confirmation of MongoDB service startup"
+		set +e
+		mongo admin --eval "help"
+		status=$?
+		set -e
+		if [ $status -eq 0 ]; then
+			echo "=> MongoDB service has started"
+			return 0
+		fi
+		sleep $SLEEP_TIME
+	done
+	echo "=> Giving up: Failed to start MongoDB service"
+	exit 1
+	}
 
 function down_test() {
-    for i in $(seq $MAX_ATTEMPTS); do
-        echo "=> Waiting till MongoDB service is stopped"
-        set +e
-        mongo admin --eval "help"
-        status=$?
-        set -e
-        if [ $status -ne 0 ]; then
-            echo "=> MongoDB service has stopped"
-            return 0
-        fi
-        sleep $SLEEP_TIME
-    done
-    echo "=> Giving up: Failed to stop MongoDB service"
-    exit 1
+	for i in $(seq $MAX_ATTEMPTS); do
+		echo "=> Waiting till MongoDB service is stopped"
+		set +e
+		mongo admin --eval "help"
+		status=$?
+		set -e
+		if [ $status -ne 0 ]; then
+			echo "=> MongoDB service has stopped"
+			return 0
+		fi
+		sleep $SLEEP_TIME
+	done
+	echo "=> Giving up: Failed to stop MongoDB service"
+	exit 1
+}
+
+# Make sure env variables don't propagate to mongod process.
+function unset_env_vars() {
+	unset MONGODB_USERNAME MONGODB_PASSWORD MONGODB_DATABASE MONGODB_ADMIN_PASSWORD
 }
 
 function create_mongodb_users() {
@@ -59,36 +65,38 @@ function create_mongodb_users() {
 	# Check if the MongoDB daemon is up.
 	up_test
 
-	# Make sure env variables don't propagate to mongod process.
-	mongo_user="$MONGODB_USERNAME" ; unset MONGODB_USERNAME
-	mongo_pass="$MONGODB_PASSWORD" ; unset MONGODB_PASSWORD
-	mongo_db="$MONGODB_DATABASE" ; unset MONGODB_DATABASE
-
 	# Create MongoDB database admin, if his password is specified with MONGODB_ADMIN_PASSWORD
-	if [ "$MONGODB_ADMIN_PASSWORD" ]; then
+	if [ -v MONGODB_ADMIN_PASSWORD ]; then
 		echo "=> Creating an admin user with a ${MONGODB_ADMIN_PASSWORD} password in MongoDB"
 		mongo admin --eval "db.addUser({user: 'admin', pwd: '$MONGODB_ADMIN_PASSWORD', roles: [ 'dbAdminAnyDatabase', 'userAdminAnyDatabase' , 'readWriteAnyDatabase' ]});"
-		unset MONGODB_ADMIN_PASSWORD
 	fi
 
 	# Create standard user with read/write permissions, in specified database ('production' by default).
-	mongo $mongo_db --eval "db.addUser({user: '${mongo_user}', pwd: '${mongo_pass}', roles: [ 'readWrite' ]});"
+	mongo $MONGODB_DATABASE --eval "db.addUser({user: '${MONGODB_USERNAME}', pwd: '${MONGODB_PASSWORD}', roles: [ 'readWrite' ]});"
 	mongod -f $MONGODB_CONFIG_PATH --shutdown
 
 	# Create a empty file which indicates that the database users were created.
-	touch /var/lib/mongodb/.mongodb_users_created
+	touch /var/lib/mongodb/data/.mongodb_users_created
 
 	# Check if the MongoDB daemon is down.
 	down_test
 }
 
-test -z "$MONGODB_USERNAME" && usage
-test -z "$MONGODB_PASSWORD" && usage
-test -z "$MONGODB_DATABASE" && usage
+if [ "$1" = "mongod" ]; then
 
-if [ ! -f /var/lib/mongodb/.mongodb_users_created ]; then
-	create_mongodb_users
+	if ! [[ -v MONGODB_USERNAME && -v MONGODB_PASSWORD && -v MONGODB_DATABASE ]]; then
+		usage
+	fi
+
+	if [ ! -f /var/lib/mongodb/data/.mongodb_users_created ]; then
+		# Create default MongoDB user and administrator.
+		create_mongodb_users
+	fi
+
+	unset_env_vars
+
+	# Start MongoDB service with enabled database authentication.
+	exec mongod -f $MONGODB_CONFIG_PATH --auth
 fi
 
-# Start MongoDB service with enabled database authentication.
-exec mongod -f $MONGODB_CONFIG_PATH --auth
+exec "$@"
