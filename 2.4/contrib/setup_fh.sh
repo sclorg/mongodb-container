e09217e96d549d134e91507354425353657c61b0
craig <craig.brookes@feedhenry.com>
2015-08-05 12:16:29 +0100

    add env vars

--- 2.4/contrib/setup_fh.sh
+++ 2.4/contrib/setup_fh.sh
@@ -5,18 +5,18 @@ source /var/lib/mongodb/common.sh
 
 function setUpMbaasDB(){
   echo "=> setting up fh-mbaas db .. ";
-  export MONGODB_DATABASE="fh-mbaas";
-  export MONGODB_USER="u-mbaas" #todo bring in from env FH_MBAAS_DB_USER
-  export MONGODB_PASSWORD="password" #todo bring in from env FH_MBAAS_DB_PASS
+  export MONGODB_DATABASE=${MONGODB_FHMBAAS_DATABASE};
+  export MONGODB_USER=${MONGODB_FHMBAAS_USER} #todo bring in from env FH_MBAAS_DB_USER
+  export MONGODB_PASSWORD=${MONGODB_FHMBAAS_PASSWORD} #todo bring in from env FH_MBAAS_DB_PASS
   mongo_create_users
 }
 
 
 function setUpReporting(){
   echo "=> setting up fh-reporting db.. ";
-  export MONGODB_DATABASE="fh-reporting";
-  export MONGODB_USER="u-reports" #todo bring in from env
-  export MONGODB_PASSWORD="password" #todo bring in from env
+  export MONGODB_DATABASE=${MONGODB_FHREPORTING_DATABASE};
+  export MONGODB_USER=${MONGODB_FHREPORTING_USER} #todo bring in from env
+  export MONGODB_PASSWORD=${MONGODB_FHREPORTING_PASSWORD} #todo bring in from env
   mongo_create_users
 }
 
