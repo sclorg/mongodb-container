255672f74f6699bc3ffb8be1abcdf8bf409ce954
Marek Skalický <mskalick@redhat.com>
2015-08-19 19:49:06 +0200

    Changed IMAGE_NAMEfor MongoDB 2.6 to rhscl/mongodb-26-$OS

--- hack/build.sh
+++ hack/build.sh
@@ -48,10 +48,9 @@ function squash {
 dirs=${VERSION:-$VERSIONS}
 
 for dir in ${dirs}; do
+  IMAGE_NAME="${BASE_IMAGE_NAME}-${dir//./}-${OS}"
   if [ "$dir" == "2.6" ]; then
-    IMAGE_NAME=rhscl/rh-mongodb${dir//./}-${OS}
-  else
-    IMAGE_NAME="${BASE_IMAGE_NAME}-${dir//./}-${OS}"
+    IMAGE_NAME=rhscl/${BASE_DIR_NAME}-${dir//./}-${OS}
   fi
 
   if [[ -v TEST_MODE ]]; then
