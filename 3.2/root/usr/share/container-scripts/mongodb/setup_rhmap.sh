37f468df9eb20b71d204833b1666dc6313189440
Gerard Ryan <gerard@ryan.lt>
2016-06-13 16:53:24 +0100

    Fix setup_rhmap.sh for 3.2
    This change makes it consistent with the `create_user` command in
    common.sh.

--- 3.2/root/usr/share/container-scripts/mongodb/setup_rhmap.sh
+++ 3.2/root/usr/share/container-scripts/mongodb/setup_rhmap.sh
@@ -2,37 +2,46 @@
 
 function setUpMbaasDB(){
   echo "=> setting up fh-mbaas db .. ";
-  mongo ${MONGODB_FHMBAAS_DATABASE} --eval \
-        "db.addUser({user: '${MONGODB_FHMBAAS_USER}', pwd: '${MONGODB_FHMBAAS_PASSWORD}', roles: [ 'readWrite' ]})"
+  local js_command="db.getSiblingDB('${MONGODB_FHMBAAS_DATABASE}').createUser({user: '${MONGODB_FHMBAAS_USER}', pwd: '${MONGODB_FHMBAAS_PASSWORD}', roles: [ 'readWrite' ]})"
+  if ! mongo admin ${1:-} --host ${2:-"localhost"} --eval "${js_command}"; then
+      echo "=> Failed to create MongoDB user: ${MONGODB_FHMBAAS_USER}"
+      exit 1
+  fi
 }
 
 
 function setUpReporting(){
   echo "=> setting up fh-reporting db.. ";
-  mongo ${MONGODB_FHREPORTING_DATABASE} --eval \
-        "db.addUser({user: '${MONGODB_FHREPORTING_USER}', pwd: '${MONGODB_FHREPORTING_PASSWORD}', roles: [ 'readWrite' ]})"
+  local js_command="db.getSiblingDB('${MONGODB_FHREPORTING_DATABASE}').createUser({user: '${MONGODB_FHREPORTING_USER}', pwd: '${MONGODB_FHREPORTING_PASSWORD}', roles: [ 'readWrite' ]})"
+  if ! mongo admin ${1:-} --host ${2:-"localhost"} --eval "${js_command}"; then
+      echo "=> Failed to create MongoDB user: ${MONGODB_FHREPORTING_USER}"
+      exit 1
+  fi
 }
 
 function setUpMetrics(){
   echo "=> setting up fh-metrics db.. ";
-  mongo ${MONGODB_FHMETRICS_DATABASE} --eval \
-        "db.addUser({user: '${MONGODB_FHMETRICS_USER}', pwd: '${MONGODB_FHMETRICS_PASSWORD}', roles: [ 'readWrite' ]})"
+  local js_command="db.getSiblingDB('${MONGODB_FHMETRICS_DATABASE}').createUser({user: '${MONGODB_FHMETRICS_USER}', pwd: '${MONGODB_FHMETRICS_PASSWORD}', roles: [ 'readWrite' ]})"
+  if ! mongo admin ${1:-} --host ${2:-"localhost"} --eval "${js_command}"; then
+      echo "=> Failed to create MongoDB user: ${MONGODB_FHMETRICS_USER}"
+      exit 1
+  fi
 }
 
 function setUpDatabases(){
   echo "=> setting up RHMAP databases";
   if [[ -v MONGODB_FHMBAAS_DATABASE && -v MONGODB_FHMBAAS_USER && -v MONGODB_FHMBAAS_PASSWORD ]]
   then
-      setUpMbaasDB
+      setUpMbaasDB "$@"
   fi
 
   if [[ -v MONGODB_FHREPORTING_DATABASE  && -v MONGODB_FHREPORTING_USER && -v MONGODB_FHREPORTING_PASSWORD ]]
   then
-      setUpReporting
+      setUpReporting "$@"
   fi
 
   if [[ -v MONGODB_FHMETRICS_DATABASE  && -v MONGODB_FHMETRICS_USER && -v MONGODB_FHMETRICS_PASSWORD ]]
   then
-      setUpMetrics
+      setUpMetrics "$@"
   fi
 }
