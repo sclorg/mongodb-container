74f88069a5b52a5debc2396062a1016d131b0032
Gerard Ryan <gerard@ryan.lt>
2016-06-13 12:53:59 +0100

    Apply changes from fheng/mongodb#2 to 3.2
    These are the changes needed to use service names instead of IP
    addresses in the 3.2 image. Originally introduced by @rhcarvalho here:
    
    https://github.com/fheng/mongodb/pull/2

--- 3.2/root/usr/share/container-scripts/mongodb/replica_supervisor.sh
+++ 3.2/root/usr/share/container-scripts/mongodb/replica_supervisor.sh
@@ -11,6 +11,13 @@ source ${CONTAINER_SCRIPTS_PATH}/common.sh
 
 echo "=> Waiting for local MongoDB to accept connections ..."
 wait_for_mongo_up
-set -x
-# Add the current container to the replSet
-mongo_add
+
+# NOTE: The `endpoints` function in `common.sh` used to return ephemeral pod
+# IPs, but now we have it return a fixed list of host names. Since the list of
+# host names is static, we do not need nor want to add/remove new members to the
+# replica set configuration as part of a pod lifecycle, therefore we comment out
+# the code below.
+
+# set-x
+# # Add the current container to the replSet
+# mongo_add
