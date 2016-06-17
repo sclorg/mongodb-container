5f7c8d771351c6b38724e76792df074d2e5c967b
Aiden Keating <akeating@redhat.com>
2016-06-17 14:19:58 +0000

    Add user creation for AAA and Supercore
    Signed-off-by: Aiden Keating <akeating@redhat.com>

--- 3.2/root/usr/share/container-scripts/mongodb/setup_rhmap.sh
+++ 3.2/root/usr/share/container-scripts/mongodb/setup_rhmap.sh
@@ -28,6 +28,24 @@ function setUpMetrics(){
   fi
 }
 
+function setUpAAA(){
+  echo "=> setting up fh-aaa db.. ";
+  local js_command="db.getSiblingDB('${MONGODB_FHAAA_DATABASE}').createUser({user: '${MONGODB_FHAAA_USER}', pwd: '${MONGODB_FHAAA_PASSWORD}', roles: [ 'readWrite' ]})"
+  if ! mongo admin ${1:-} --host ${2:-"localhost"} --eval "${js_command}"; then
+      echo "=> Failed to create MongoDB user: ${MONGODB_FHAAA_USER}"
+      exit 1
+  fi
+}
+
+function setUpSupercore(){
+  echo "=> setting up fh-SUPERCORE db.. ";
+  local js_command="db.getSiblingDB('${MONGODB_FHSUPERCORE_DATABASE}').createUser({user: '${MONGODB_FHSUPERCORE_USER}', pwd: '${MONGODB_FHSUPERCORE_PASSWORD}', roles: [ 'readWrite' ]})"
+  if ! mongo admin ${1:-} --host ${2:-"localhost"} --eval "${js_command}"; then
+      echo "=> Failed to create MongoDB user: ${MONGODB_FHSUPERCORE_USER}"
+      exit 1
+  fi
+}
+
 function setUpDatabases(){
   echo "=> setting up RHMAP databases";
   if [[ -v MONGODB_FHMBAAS_DATABASE && -v MONGODB_FHMBAAS_USER && -v MONGODB_FHMBAAS_PASSWORD ]]
@@ -43,5 +61,15 @@ function setUpDatabases(){
   if [[ -v MONGODB_FHMETRICS_DATABASE  && -v MONGODB_FHMETRICS_USER && -v MONGODB_FHMETRICS_PASSWORD ]]
   then
       setUpMetrics "$@"
+  fi
+
+  if [[ -v MONGODB_FHAAA_DATABASE && -v MONGODB_FHAAA_USER && -v MONGODB_FHAAA_PASSWORD ]]
+  then
+      setUpAAA "$@"
+  fi
+
+  if [[ -v MONGODB_FHSUPERCORE_DATABASE && -v MONGODB_FHSUPERCORE_USER && -v MONGODB_FHSUPERCORE_PASSWORD ]]
+  then
+      setUpSupercore "$@"
   fi
 }
