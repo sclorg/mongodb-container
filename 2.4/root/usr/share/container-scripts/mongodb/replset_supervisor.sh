sclorg/mongodb-container

commit df104523417431ac013733b5478e483124b048c8

Split main scripts for standalone server and replication.

2.4/root/usr/bin/run-mongod-replication
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# Run mongod which connect to replSet
+# $1 - "initiate" (optional) -> this mongod will initiate a replSet
+#
+# If this script is terminated it removes mongod from replSet
 
 set -o errexit
 set -o nounset
@@ -12,8 +17,8 @@ function usage() {
   echo "  MONGODB_PASSWORD"
   echo "  MONGODB_DATABASE"
   echo "  MONGODB_ADMIN_PASSWORD"
-  echo "Optional variables:"
   echo "  MONGODB_REPLICA_NAME"
+  echo "Optional variables:"
   echo "  MONGODB_INITIAL_REPLICA_COUNT"
   echo "MongoDB settings:"
   echo "  MONGODB_NOPREALLOC (default: true)"
@@ -50,29 +55,21 @@ fi
 # Need to cache the container address for the cleanup
 cache_container_addr
 mongo_common_args="-f $MONGODB_CONFIG_PATH"
-if ! [[ -v MONGODB_USER && -v MONGODB_PASSWORD && -v MONGODB_DATABASE && -v MONGODB_ADMIN_PASSWORD ]]; then
-  usage
+if ! [[ -v MONGODB_USER && -v MONGODB_PASSWORD && -v MONGODB_DATABASE && -v MONGODB_ADMIN_PASSWORD && -v MONGODB_REPLICA_NAME ]]; then
+    usage
 fi
 
-# Run the MongoDB in 'standalone' mode
-mongod $mongo_common_args &
-wait_for_mongo_up
-js_command="db.system.users.count({'user':'admin', 'db':'admin'})"
-if [ "$(mongo admin --quiet --eval "$js_command")" == "1" ]; then
-  echo "=> Admin user is already created. Resetting password ..."
-  mongo_reset_admin
-else
-  mongo_create_admin
-fi
-js_command="db.system.users.count({'user':'${MONGODB_USER}', 'db':'${MONGODB_DATABASE}'})"
-if [ "$(mongo admin --quiet --eval "$js_command")" == "1" ]; then
-  echo "=> MONGODB_USER user is already created. Resetting password ..."
-  mongo_reset_user
-else
-  mongo_create_user
-fi
-# Restart the MongoDB daemon to bind on all interfaces
-mongod $mongo_common_args --shutdown
-wait_for_mongo_down
-unset_env_vars
-exec mongod $mongo_common_args --auth
+# Run the MongoDB in 'replicated' mode
+setup_keyfile
+
+# Run the supervisor in background to manage replset
+${CONTAINER_SCRIPTS_PATH}/replset_supervisor.sh "${1:-}" "$$" &
+
+# Run `unset_env_vars` and `mongod` in a subshell because
+# MONGODB_ADMIN_PASSWORD should still be defined when the trapped call to
+# `cleanup` references it.
+(
+  unset_env_vars
+  mongod $mongo_common_args --replSet "${MONGODB_REPLICA_NAME}"
+) &
+wait

2.4/root/usr/share/container-scripts/mongodb/replset_supervisor.sh
@@ -1,0 +1,84 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source ${CONTAINER_SCRIPTS_PATH}/common.sh
+
+# Initiate replicaset and exit
+if [[ "$1" == "initiate" ]]; then
+  main_process_id=$2
+  current_endpoints=$(endpoints)
+  if [ -n "${MONGODB_INITIAL_REPLICA_COUNT:-}" ]; then
+    echo -n "=> Waiting for $MONGODB_INITIAL_REPLICA_COUNT MongoDB endpoints ..."
+    while [[ "$(echo "${current_endpoints}" | wc -l)" -lt ${MONGODB_INITIAL_REPLICA_COUNT} ]]; do
+      sleep 2
+      current_endpoints=$(endpoints)
+    done
+  else
+    echo "Attention: MONGODB_INITIAL_REPLICA_COUNT is not set and it could lead to a improperly configured replica set."
+    echo "To fix this, set MONGODB_INITIAL_REPLICA_COUNT variable to the number of members in the replica set in"
+    echo "the configuration of post deployment hook."
+
+    echo -n "=> Waiting for MongoDB endpoints ..."
+    while [ -z "${current_endpoints}" ]; do
+      sleep 2
+      current_endpoints=$(endpoints)
+    done
+  fi
+  echo "${current_endpoints}"
+
+  # Let initialize the first member of the cluster
+  mongo_node="$(echo -n ${current_endpoints} | cut -d ' ' -f 1):${CONTAINER_PORT}"
+
+  echo "=> Waiting for all endpoints to accept connections..."
+  for node in ${current_endpoints}; do
+    wait_for_mongo_up ${node} &>/dev/null
+  done
+
+  echo "=> Initiating the replSet ${MONGODB_REPLICA_NAME} ..."
+  # This will perform the 'rs.initiate()' command on the current MongoDB.
+  mongo_initiate "${current_endpoints}"
+
+  echo "=> Creating MongoDB users ..."
+  mongo_create_admin
+  mongo_create_user "-u admin -p ${MONGODB_ADMIN_PASSWORD}"
+
+  echo "=> Waiting for replication to finish ..."
+  # TODO: Replace this with polling or a Mongo script that will check if all
+  #       members of the cluster are now properly replicated (user accounts are
+  #       created on all members).
+  sleep 10
+
+  # Some commands will force MongoDB client to re-connect. This is not working
+  # well in combination with '--eval'. In that case the 'mongo' command will fail
+  # with return code 254.
+  echo "=> Initiate Pod giving up the PRIMARY role ..."
+  mongo admin -u admin -p "${MONGODB_ADMIN_PASSWORD}" --quiet --eval "rs.stepDown(120);" &>/dev/null || true
+
+  # Wait till the new PRIMARY member is elected
+  echo "=> Waiting for the new PRIMARY to be elected ..."
+  mongo admin -u admin -p "${MONGODB_ADMIN_PASSWORD}" --quiet --host ${mongo_node} --eval "var done=false;while(done==false){var members=rs.status().members;for(i=0;i<members.length;i++){if(members[i].stateStr=='PRIMARY' && members[i].name!='$(mongo_addr)'){done=true}};sleep(500)};" &>/dev/null
+
+  # Remove the initialization container MongoDB from cluster and shutdown
+  echo "=> The new PRIMARY member elected, shutting down current member ..."
+  mongo_remove
+
+  mongod -f ${MONGODB_CONFIG_PATH} --shutdown &>/dev/null
+  wait_for_mongo_down
+
+  echo "=> Successfully initialized replSet"
+
+  # Exit this pod
+  kill ${main_process_id}
+
+# Try to add node into replicaset
+else
+  echo "=> Waiting for local MongoDB to accept connections ..."
+  wait_for_mongo_up
+  set -x
+  # Add the current container to the replSet
+  mongo_add
+
+fi

2.6/root/usr/bin/run-mongod-replication
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# Run mongod which connect to replSet
+# $1 - "initiate" (optional) -> this mongod will initiate a replSet
+#
+# If this script is terminated it removes mongod from replSet
 
 set -o errexit
 set -o nounset
@@ -12,8 +17,8 @@ function usage() {
   echo "  MONGODB_PASSWORD"
   echo "  MONGODB_DATABASE"
   echo "  MONGODB_ADMIN_PASSWORD"
-  echo "Optional variables:"
   echo "  MONGODB_REPLICA_NAME"
+  echo "Optional variables:"
   echo "  MONGODB_INITIAL_REPLICA_COUNT"
   echo "MongoDB settings:"
   echo "  MONGODB_NOPREALLOC (default: true)"
@@ -50,29 +55,21 @@ fi
 # Need to cache the container address for the cleanup
 cache_container_addr
 mongo_common_args="-f $MONGODB_CONFIG_PATH"
-if ! [[ -v MONGODB_USER && -v MONGODB_PASSWORD && -v MONGODB_DATABASE && -v MONGODB_ADMIN_PASSWORD ]]; then
-  usage
+if ! [[ -v MONGODB_USER && -v MONGODB_PASSWORD && -v MONGODB_DATABASE && -v MONGODB_ADMIN_PASSWORD && -v MONGODB_REPLICA_NAME ]]; then
+    usage
 fi
 
-# Run the MongoDB in 'standalone' mode
-mongod $mongo_common_args &
-wait_for_mongo_up
-js_command="db.system.users.count({'user':'admin', 'db':'admin'})"
-if [ "$(mongo admin --quiet --eval "$js_command")" == "1" ]; then
-  echo "=> Admin user is already created. Resetting password ..."
-  mongo_reset_admin
-else
-  mongo_create_admin
-fi
-js_command="db.system.users.count({'user':'${MONGODB_USER}', 'db':'${MONGODB_DATABASE}'})"
-if [ "$(mongo admin --quiet --eval "$js_command")" == "1" ]; then
-  echo "=> MONGODB_USER user is already created. Resetting password ..."
-  mongo_reset_user
-else
-  mongo_create_user
-fi
-# Restart the MongoDB daemon to bind on all interfaces
-mongod $mongo_common_args --shutdown
-wait_for_mongo_down
-unset_env_vars
-exec mongod $mongo_common_args --auth
+# Run the MongoDB in 'replicated' mode
+setup_keyfile
+
+# Run the supervisor in background to manage replset
+${CONTAINER_SCRIPTS_PATH}/replset_supervisor.sh "${1:-}" "$$" &
+
+# Run `unset_env_vars` and `mongod` in a subshell because
+# MONGODB_ADMIN_PASSWORD should still be defined when the trapped call to
+# `cleanup` references it.
+(
+  unset_env_vars
+  mongod $mongo_common_args --replSet "${MONGODB_REPLICA_NAME}"
+) &
+wait

2.6/root/usr/share/container-scripts/mongodb/replset_supervisor.sh
@@ -1,0 +1,84 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source ${CONTAINER_SCRIPTS_PATH}/common.sh
+
+# Initiate replicaset and exit
+if [[ "$1" == "initiate" ]]; then
+  main_process_id=$2
+  current_endpoints=$(endpoints)
+  if [ -n "${MONGODB_INITIAL_REPLICA_COUNT:-}" ]; then
+    echo -n "=> Waiting for $MONGODB_INITIAL_REPLICA_COUNT MongoDB endpoints ..."
+    while [[ "$(echo "${current_endpoints}" | wc -l)" -lt ${MONGODB_INITIAL_REPLICA_COUNT} ]]; do
+      sleep 2
+      current_endpoints=$(endpoints)
+    done
+  else
+    echo "Attention: MONGODB_INITIAL_REPLICA_COUNT is not set and it could lead to a improperly configured replica set."
+    echo "To fix this, set MONGODB_INITIAL_REPLICA_COUNT variable to the number of members in the replica set in"
+    echo "the configuration of post deployment hook."
+
+    echo -n "=> Waiting for MongoDB endpoints ..."
+    while [ -z "${current_endpoints}" ]; do
+      sleep 2
+      current_endpoints=$(endpoints)
+    done
+  fi
+  echo "${current_endpoints}"
+
+  # Let initialize the first member of the cluster
+  mongo_node="$(echo -n ${current_endpoints} | cut -d ' ' -f 1):${CONTAINER_PORT}"
+
+  echo "=> Waiting for all endpoints to accept connections..."
+  for node in ${current_endpoints}; do
+    wait_for_mongo_up ${node} &>/dev/null
+  done
+
+  echo "=> Initiating the replSet ${MONGODB_REPLICA_NAME} ..."
+  # This will perform the 'rs.initiate()' command on the current MongoDB.
+  mongo_initiate "${current_endpoints}"
+
+  echo "=> Creating MongoDB users ..."
+  mongo_create_admin
+  mongo_create_user "-u admin -p ${MONGODB_ADMIN_PASSWORD}"
+
+  echo "=> Waiting for replication to finish ..."
+  # TODO: Replace this with polling or a Mongo script that will check if all
+  #       members of the cluster are now properly replicated (user accounts are
+  #       created on all members).
+  sleep 10
+
+  # Some commands will force MongoDB client to re-connect. This is not working
+  # well in combination with '--eval'. In that case the 'mongo' command will fail
+  # with return code 254.
+  echo "=> Initiate Pod giving up the PRIMARY role ..."
+  mongo admin -u admin -p "${MONGODB_ADMIN_PASSWORD}" --quiet --eval "rs.stepDown(120);" &>/dev/null || true
+
+  # Wait till the new PRIMARY member is elected
+  echo "=> Waiting for the new PRIMARY to be elected ..."
+  mongo admin -u admin -p "${MONGODB_ADMIN_PASSWORD}" --quiet --host ${mongo_node} --eval "var done=false;while(done==false){var members=rs.status().members;for(i=0;i<members.length;i++){if(members[i].stateStr=='PRIMARY' && members[i].name!='$(mongo_addr)'){done=true}};sleep(500)};" &>/dev/null
+
+  # Remove the initialization container MongoDB from cluster and shutdown
+  echo "=> The new PRIMARY member elected, shutting down current member ..."
+  mongo_remove
+
+  mongod -f ${MONGODB_CONFIG_PATH} --shutdown &>/dev/null
+  wait_for_mongo_down
+
+  echo "=> Successfully initialized replSet"
+
+  # Exit this pod
+  kill ${main_process_id}
+
+# Try to add node into replicaset
+else
+  echo "=> Waiting for local MongoDB to accept connections ..."
+  wait_for_mongo_up
+  set -x
+  # Add the current container to the replSet
+  mongo_add
+
+fi

3.0-upg/root/usr/bin/run-mongod-replication
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# Run mongod which connect to replSet
+# $1 - "initiate" (optional) -> this mongod will initiate a replSet
+#
+# If this script is terminated it removes mongod from replSet
 
 set -o errexit
 set -o nounset
@@ -12,8 +17,8 @@ function usage() {
   echo "  MONGODB_PASSWORD"
   echo "  MONGODB_DATABASE"
   echo "  MONGODB_ADMIN_PASSWORD"
-  echo "Optional variables:"
   echo "  MONGODB_REPLICA_NAME"
+  echo "Optional variables:"
   echo "  MONGODB_INITIAL_REPLICA_COUNT"
   echo "MongoDB settings:"
   echo "  MONGODB_NOPREALLOC (default: true)"
@@ -50,29 +55,21 @@ fi
 # Need to cache the container address for the cleanup
 cache_container_addr
 mongo_common_args="-f $MONGODB_CONFIG_PATH"
-if ! [[ -v MONGODB_USER && -v MONGODB_PASSWORD && -v MONGODB_DATABASE && -v MONGODB_ADMIN_PASSWORD ]]; then
-  usage
+if ! [[ -v MONGODB_USER && -v MONGODB_PASSWORD && -v MONGODB_DATABASE && -v MONGODB_ADMIN_PASSWORD && -v MONGODB_REPLICA_NAME ]]; then
+    usage
 fi
 
-# Run the MongoDB in 'standalone' mode
-mongod $mongo_common_args &
-wait_for_mongo_up
-js_command="db.system.users.count({'user':'admin', 'db':'admin'})"
-if [ "$(mongo admin --quiet --eval "$js_command")" == "1" ]; then
-  echo "=> Admin user is already created. Resetting password ..."
-  mongo_reset_admin
-else
-  mongo_create_admin
-fi
-js_command="db.system.users.count({'user':'${MONGODB_USER}', 'db':'${MONGODB_DATABASE}'})"
-if [ "$(mongo admin --quiet --eval "$js_command")" == "1" ]; then
-  echo "=> MONGODB_USER user is already created. Resetting password ..."
-  mongo_reset_user
-else
-  mongo_create_user
-fi
-# Restart the MongoDB daemon to bind on all interfaces
-mongod $mongo_common_args --shutdown
-wait_for_mongo_down
-unset_env_vars
-exec mongod $mongo_common_args --auth
+# Run the MongoDB in 'replicated' mode
+setup_keyfile
+
+# Run the supervisor in background to manage replset
+${CONTAINER_SCRIPTS_PATH}/replset_supervisor.sh "${1:-}" "$$" &
+
+# Run `unset_env_vars` and `mongod` in a subshell because
+# MONGODB_ADMIN_PASSWORD should still be defined when the trapped call to
+# `cleanup` references it.
+(
+  unset_env_vars
+  mongod $mongo_common_args --replSet "${MONGODB_REPLICA_NAME}"
+) &
+wait

3.0-upg/root/usr/share/container-scripts/mongodb/replset_supervisor.sh
@@ -1,0 +1,84 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source ${CONTAINER_SCRIPTS_PATH}/common.sh
+
+# Initiate replicaset and exit
+if [[ "$1" == "initiate" ]]; then
+  main_process_id=$2
+  current_endpoints=$(endpoints)
+  if [ -n "${MONGODB_INITIAL_REPLICA_COUNT:-}" ]; then
+    echo -n "=> Waiting for $MONGODB_INITIAL_REPLICA_COUNT MongoDB endpoints ..."
+    while [[ "$(echo "${current_endpoints}" | wc -l)" -lt ${MONGODB_INITIAL_REPLICA_COUNT} ]]; do
+      sleep 2
+      current_endpoints=$(endpoints)
+    done
+  else
+    echo "Attention: MONGODB_INITIAL_REPLICA_COUNT is not set and it could lead to a improperly configured replica set."
+    echo "To fix this, set MONGODB_INITIAL_REPLICA_COUNT variable to the number of members in the replica set in"
+    echo "the configuration of post deployment hook."
+
+    echo -n "=> Waiting for MongoDB endpoints ..."
+    while [ -z "${current_endpoints}" ]; do
+      sleep 2
+      current_endpoints=$(endpoints)
+    done
+  fi
+  echo "${current_endpoints}"
+
+  # Let initialize the first member of the cluster
+  mongo_node="$(echo -n ${current_endpoints} | cut -d ' ' -f 1):${CONTAINER_PORT}"
+
+  echo "=> Waiting for all endpoints to accept connections..."
+  for node in ${current_endpoints}; do
+    wait_for_mongo_up ${node} &>/dev/null
+  done
+
+  echo "=> Initiating the replSet ${MONGODB_REPLICA_NAME} ..."
+  # This will perform the 'rs.initiate()' command on the current MongoDB.
+  mongo_initiate "${current_endpoints}"
+
+  echo "=> Creating MongoDB users ..."
+  mongo_create_admin
+  mongo_create_user "-u admin -p ${MONGODB_ADMIN_PASSWORD}"
+
+  echo "=> Waiting for replication to finish ..."
+  # TODO: Replace this with polling or a Mongo script that will check if all
+  #       members of the cluster are now properly replicated (user accounts are
+  #       created on all members).
+  sleep 10
+
+  # Some commands will force MongoDB client to re-connect. This is not working
+  # well in combination with '--eval'. In that case the 'mongo' command will fail
+  # with return code 254.
+  echo "=> Initiate Pod giving up the PRIMARY role ..."
+  mongo admin -u admin -p "${MONGODB_ADMIN_PASSWORD}" --quiet --eval "rs.stepDown(120);" &>/dev/null || true
+
+  # Wait till the new PRIMARY member is elected
+  echo "=> Waiting for the new PRIMARY to be elected ..."
+  mongo admin -u admin -p "${MONGODB_ADMIN_PASSWORD}" --quiet --host ${mongo_node} --eval "var done=false;while(done==false){var members=rs.status().members;for(i=0;i<members.length;i++){if(members[i].stateStr=='PRIMARY' && members[i].name!='$(mongo_addr)'){done=true}};sleep(500)};" &>/dev/null
+
+  # Remove the initialization container MongoDB from cluster and shutdown
+  echo "=> The new PRIMARY member elected, shutting down current member ..."
+  mongo_remove
+
+  mongod -f ${MONGODB_CONFIG_PATH} --shutdown &>/dev/null
+  wait_for_mongo_down
+
+  echo "=> Successfully initialized replSet"
+
+  # Exit this pod
+  kill ${main_process_id}
+
+# Try to add node into replicaset
+else
+  echo "=> Waiting for local MongoDB to accept connections ..."
+  wait_for_mongo_up
+  set -x
+  # Add the current container to the replSet
+  mongo_add
+
+fi

3.2/root/usr/bin/run-mongod-replication
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# Run mongod which connect to replSet
+# $1 - "initiate" (optional) -> this mongod will initiate a replSet
+#
+# If this script is terminated it removes mongod from replSet
 
 set -o errexit
 set -o nounset
@@ -12,8 +17,8 @@ function usage() {
   echo "  MONGODB_PASSWORD"
   echo "  MONGODB_DATABASE"
   echo "  MONGODB_ADMIN_PASSWORD"
-  echo "Optional variables:"
   echo "  MONGODB_REPLICA_NAME"
+  echo "Optional variables:"
   echo "  MONGODB_INITIAL_REPLICA_COUNT"
   echo "MongoDB settings:"
   echo "  MONGODB_NOPREALLOC (default: true)"
@@ -50,29 +55,21 @@ fi
 # Need to cache the container address for the cleanup
 cache_container_addr
 mongo_common_args="-f $MONGODB_CONFIG_PATH"
-if ! [[ -v MONGODB_USER && -v MONGODB_PASSWORD && -v MONGODB_DATABASE && -v MONGODB_ADMIN_PASSWORD ]]; then
-  usage
+if ! [[ -v MONGODB_USER && -v MONGODB_PASSWORD && -v MONGODB_DATABASE && -v MONGODB_ADMIN_PASSWORD && -v MONGODB_REPLICA_NAME ]]; then
+    usage
 fi
 
-# Run the MongoDB in 'standalone' mode
-mongod $mongo_common_args &
-wait_for_mongo_up
-js_command="db.system.users.count({'user':'admin', 'db':'admin'})"
-if [ "$(mongo admin --quiet --eval "$js_command")" == "1" ]; then
-  echo "=> Admin user is already created. Resetting password ..."
-  mongo_reset_admin
-else
-  mongo_create_admin
-fi
-js_command="db.system.users.count({'user':'${MONGODB_USER}', 'db':'${MONGODB_DATABASE}'})"
-if [ "$(mongo admin --quiet --eval "$js_command")" == "1" ]; then
-  echo "=> MONGODB_USER user is already created. Resetting password ..."
-  mongo_reset_user
-else
-  mongo_create_user
-fi
-# Restart the MongoDB daemon to bind on all interfaces
-mongod $mongo_common_args --shutdown
-wait_for_mongo_down
-unset_env_vars
-exec mongod $mongo_common_args --auth
+# Run the MongoDB in 'replicated' mode
+setup_keyfile
+
+# Run the supervisor in background to manage replset
+${CONTAINER_SCRIPTS_PATH}/replset_supervisor.sh "${1:-}" "$$" &
+
+# Run `unset_env_vars` and `mongod` in a subshell because
+# MONGODB_ADMIN_PASSWORD should still be defined when the trapped call to
+# `cleanup` references it.
+(
+  unset_env_vars
+  mongod $mongo_common_args --replSet "${MONGODB_REPLICA_NAME}"
+) &
+wait

3.2/root/usr/share/container-scripts/mongodb/replset_supervisor.sh
@@ -1,0 +1,84 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source ${CONTAINER_SCRIPTS_PATH}/common.sh
+
+# Initiate replicaset and exit
+if [[ "$1" == "initiate" ]]; then
+  main_process_id=$2
+  current_endpoints=$(endpoints)
+  if [ -n "${MONGODB_INITIAL_REPLICA_COUNT:-}" ]; then
+    echo -n "=> Waiting for $MONGODB_INITIAL_REPLICA_COUNT MongoDB endpoints ..."
+    while [[ "$(echo "${current_endpoints}" | wc -l)" -lt ${MONGODB_INITIAL_REPLICA_COUNT} ]]; do
+      sleep 2
+      current_endpoints=$(endpoints)
+    done
+  else
+    echo "Attention: MONGODB_INITIAL_REPLICA_COUNT is not set and it could lead to a improperly configured replica set."
+    echo "To fix this, set MONGODB_INITIAL_REPLICA_COUNT variable to the number of members in the replica set in"
+    echo "the configuration of post deployment hook."
+
+    echo -n "=> Waiting for MongoDB endpoints ..."
+    while [ -z "${current_endpoints}" ]; do
+      sleep 2
+      current_endpoints=$(endpoints)
+    done
+  fi
+  echo "${current_endpoints}"
+
+  # Let initialize the first member of the cluster
+  mongo_node="$(echo -n ${current_endpoints} | cut -d ' ' -f 1):${CONTAINER_PORT}"
+
+  echo "=> Waiting for all endpoints to accept connections..."
+  for node in ${current_endpoints}; do
+    wait_for_mongo_up ${node} &>/dev/null
+  done
+
+  echo "=> Initiating the replSet ${MONGODB_REPLICA_NAME} ..."
+  # This will perform the 'rs.initiate()' command on the current MongoDB.
+  mongo_initiate "${current_endpoints}"
+
+  echo "=> Creating MongoDB users ..."
+  mongo_create_admin
+  mongo_create_user "-u admin -p ${MONGODB_ADMIN_PASSWORD}"
+
+  echo "=> Waiting for replication to finish ..."
+  # TODO: Replace this with polling or a Mongo script that will check if all
+  #       members of the cluster are now properly replicated (user accounts are
+  #       created on all members).
+  sleep 10
+
+  # Some commands will force MongoDB client to re-connect. This is not working
+  # well in combination with '--eval'. In that case the 'mongo' command will fail
+  # with return code 254.
+  echo "=> Initiate Pod giving up the PRIMARY role ..."
+  mongo admin -u admin -p "${MONGODB_ADMIN_PASSWORD}" --quiet --eval "rs.stepDown(120);" &>/dev/null || true
+
+  # Wait till the new PRIMARY member is elected
+  echo "=> Waiting for the new PRIMARY to be elected ..."
+  mongo admin -u admin -p "${MONGODB_ADMIN_PASSWORD}" --quiet --host ${mongo_node} --eval "var done=false;while(done==false){var members=rs.status().members;for(i=0;i<members.length;i++){if(members[i].stateStr=='PRIMARY' && members[i].name!='$(mongo_addr)'){done=true}};sleep(500)};" &>/dev/null
+
+  # Remove the initialization container MongoDB from cluster and shutdown
+  echo "=> The new PRIMARY member elected, shutting down current member ..."
+  mongo_remove
+
+  mongod -f ${MONGODB_CONFIG_PATH} --shutdown &>/dev/null
+  wait_for_mongo_down
+
+  echo "=> Successfully initialized replSet"
+
+  # Exit this pod
+  kill ${main_process_id}
+
+# Try to add node into replicaset
+else
+  echo "=> Waiting for local MongoDB to accept connections ..."
+  wait_for_mongo_up
+  set -x
+  # Add the current container to the replSet
+  mongo_add
+
+fi

commit c8299ce7d7fbad82185ad80746a7392815a029bf

run-mongod-replication: fix races.
This fixes MongoDB replication that was broken by
df104523417431ac013733b5478e483124b048c8 commit.

Before this change we run the replset_supervisor.sh script in the
background and after that run the mongodb daemon. Because
replset_supervisor.sh script connect to mongo it was possible that
database wasn't ready yet and script was failing. I updated the script
for waiting when mongo will accept connections.

Replica set started working but only for a short time and later it
stopped working again because mongo process was killed to early. To fix
this I removed the code from replset_supervisor.sh script that kills
mongo.

2.4/root/usr/bin/run-mongod-replication
@@ -63,7 +63,7 @@ fi
 setup_keyfile
 
 # Run the supervisor in background to manage replset
-${CONTAINER_SCRIPTS_PATH}/replset_supervisor.sh "${1:-}" "$$" &
+${CONTAINER_SCRIPTS_PATH}/replset_supervisor.sh "${1:-}" &
 
 # Run `unset_env_vars` and `mongod` in a subshell because
 # MONGODB_ADMIN_PASSWORD should still be defined when the trapped call to

2.4/root/usr/share/container-scripts/mongodb/replset_supervisor.sh
@@ -8,7 +8,6 @@ source ${CONTAINER_SCRIPTS_PATH}/common.sh
 
 # Initiate replicaset and exit
 if [[ "$1" == "initiate" ]]; then
-  main_process_id=$2
   current_endpoints=$(endpoints)
   if [ -n "${MONGODB_INITIAL_REPLICA_COUNT:-}" ]; then
     echo -n "=> Waiting for $MONGODB_INITIAL_REPLICA_COUNT MongoDB endpoints ..."
@@ -36,6 +35,9 @@ if [[ "$1" == "initiate" ]]; then
   for node in ${current_endpoints}; do
     wait_for_mongo_up ${node} &>/dev/null
   done
+
+  echo "=> Waiting for local MongoDB to accept connections ..."
+  wait_for_mongo_up &>/dev/null
 
   echo "=> Initiating the replSet ${MONGODB_REPLICA_NAME} ..."
   # This will perform the 'rs.initiate()' command on the current MongoDB.
@@ -69,9 +71,6 @@ if [[ "$1" == "initiate" ]]; then
   wait_for_mongo_down
 
   echo "=> Successfully initialized replSet"
-
-  # Exit this pod
-  kill ${main_process_id}
 
 # Try to add node into replicaset
 else

2.6/root/usr/bin/run-mongod-replication
@@ -63,7 +63,7 @@ fi
 setup_keyfile
 
 # Run the supervisor in background to manage replset
-${CONTAINER_SCRIPTS_PATH}/replset_supervisor.sh "${1:-}" "$$" &
+${CONTAINER_SCRIPTS_PATH}/replset_supervisor.sh "${1:-}" &
 
 # Run `unset_env_vars` and `mongod` in a subshell because
 # MONGODB_ADMIN_PASSWORD should still be defined when the trapped call to

2.6/root/usr/share/container-scripts/mongodb/replset_supervisor.sh
@@ -8,7 +8,6 @@ source ${CONTAINER_SCRIPTS_PATH}/common.sh
 
 # Initiate replicaset and exit
 if [[ "$1" == "initiate" ]]; then
-  main_process_id=$2
   current_endpoints=$(endpoints)
   if [ -n "${MONGODB_INITIAL_REPLICA_COUNT:-}" ]; then
     echo -n "=> Waiting for $MONGODB_INITIAL_REPLICA_COUNT MongoDB endpoints ..."
@@ -36,6 +35,9 @@ if [[ "$1" == "initiate" ]]; then
   for node in ${current_endpoints}; do
     wait_for_mongo_up ${node} &>/dev/null
   done
+
+  echo "=> Waiting for local MongoDB to accept connections ..."
+  wait_for_mongo_up &>/dev/null
 
   echo "=> Initiating the replSet ${MONGODB_REPLICA_NAME} ..."
   # This will perform the 'rs.initiate()' command on the current MongoDB.
@@ -69,9 +71,6 @@ if [[ "$1" == "initiate" ]]; then
   wait_for_mongo_down
 
   echo "=> Successfully initialized replSet"
-
-  # Exit this pod
-  kill ${main_process_id}
 
 # Try to add node into replicaset
 else

3.0-upg/root/usr/bin/run-mongod-replication
@@ -63,7 +63,7 @@ fi
 setup_keyfile
 
 # Run the supervisor in background to manage replset
-${CONTAINER_SCRIPTS_PATH}/replset_supervisor.sh "${1:-}" "$$" &
+${CONTAINER_SCRIPTS_PATH}/replset_supervisor.sh "${1:-}" &
 
 # Run `unset_env_vars` and `mongod` in a subshell because
 # MONGODB_ADMIN_PASSWORD should still be defined when the trapped call to

3.0-upg/root/usr/share/container-scripts/mongodb/replset_supervisor.sh
@@ -8,7 +8,6 @@ source ${CONTAINER_SCRIPTS_PATH}/common.sh
 
 # Initiate replicaset and exit
 if [[ "$1" == "initiate" ]]; then
-  main_process_id=$2
   current_endpoints=$(endpoints)
   if [ -n "${MONGODB_INITIAL_REPLICA_COUNT:-}" ]; then
     echo -n "=> Waiting for $MONGODB_INITIAL_REPLICA_COUNT MongoDB endpoints ..."
@@ -36,6 +35,9 @@ if [[ "$1" == "initiate" ]]; then
   for node in ${current_endpoints}; do
     wait_for_mongo_up ${node} &>/dev/null
   done
+
+  echo "=> Waiting for local MongoDB to accept connections ..."
+  wait_for_mongo_up &>/dev/null
 
   echo "=> Initiating the replSet ${MONGODB_REPLICA_NAME} ..."
   # This will perform the 'rs.initiate()' command on the current MongoDB.
@@ -69,9 +71,6 @@ if [[ "$1" == "initiate" ]]; then
   wait_for_mongo_down
 
   echo "=> Successfully initialized replSet"
-
-  # Exit this pod
-  kill ${main_process_id}
 
 # Try to add node into replicaset
 else

3.2/root/usr/bin/run-mongod-replication
@@ -63,7 +63,7 @@ fi
 setup_keyfile
 
 # Run the supervisor in background to manage replset
-${CONTAINER_SCRIPTS_PATH}/replset_supervisor.sh "${1:-}" "$$" &
+${CONTAINER_SCRIPTS_PATH}/replset_supervisor.sh "${1:-}" &
 
 # Run `unset_env_vars` and `mongod` in a subshell because
 # MONGODB_ADMIN_PASSWORD should still be defined when the trapped call to

3.2/root/usr/share/container-scripts/mongodb/replset_supervisor.sh
@@ -8,7 +8,6 @@ source ${CONTAINER_SCRIPTS_PATH}/common.sh
 
 # Initiate replicaset and exit
 if [[ "$1" == "initiate" ]]; then
-  main_process_id=$2
   current_endpoints=$(endpoints)
   if [ -n "${MONGODB_INITIAL_REPLICA_COUNT:-}" ]; then
     echo -n "=> Waiting for $MONGODB_INITIAL_REPLICA_COUNT MongoDB endpoints ..."
@@ -36,6 +35,9 @@ if [[ "$1" == "initiate" ]]; then
   for node in ${current_endpoints}; do
     wait_for_mongo_up ${node} &>/dev/null
   done
+
+  echo "=> Waiting for local MongoDB to accept connections ..."
+  wait_for_mongo_up &>/dev/null
 
   echo "=> Initiating the replSet ${MONGODB_REPLICA_NAME} ..."
   # This will perform the 'rs.initiate()' command on the current MongoDB.
@@ -69,9 +71,6 @@ if [[ "$1" == "initiate" ]]; then
   wait_for_mongo_down
 
   echo "=> Successfully initialized replSet"
-
-  # Exit this pod
-  kill ${main_process_id}
 
 # Try to add node into replicaset
 else