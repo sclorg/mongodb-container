391317a6414098b375e2744fcfbfd4c849d53954
Slava Semushin <vsemushi@redhat.com>
2016-09-29 17:30:25 +0200

    common.sh(setup_keyfile): improve error message when home directory isn't writable.

--- 3.0-upg/root/usr/share/container-scripts/mongodb/common.sh
+++ 3.0-upg/root/usr/share/container-scripts/mongodb/common.sh
@@ -246,6 +246,15 @@ function setup_keyfile() {
     echo "ERROR: You have to provide the 'keyfile' value in MONGODB_KEYFILE_VALUE"
     exit 1
   fi
+  local keyfile_dir
+  keyfile_dir="$(dirname "$MONGODB_KEYFILE_PATH")"
+  if [ ! -w "$keyfile_dir" ]; then
+    echo >&2 "ERROR: Couldn't create ${MONGODB_KEYFILE_PATH}"
+    echo >&2 "CAUSE: current user doesn't have permissions for writing to ${keyfile_dir} directory"
+    echo >&2 "DETAILS: current user id = $(id -u), user groups: $(id -G)"
+    echo >&2 "DETAILS: directory permissions: $(stat -c '%A owned by %u:%g' "${keyfile_dir}")"
+    exit 1
+  fi
   echo ${MONGODB_KEYFILE_VALUE} > ${MONGODB_KEYFILE_PATH}
   chmod 0600 ${MONGODB_KEYFILE_PATH}
   mongo_common_args+=" --keyFile ${MONGODB_KEYFILE_PATH}"
