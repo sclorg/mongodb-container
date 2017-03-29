d0441ab8b7a6816764a18c1a2bfa1bae23db1a13
Marek Skalický <mskalick@redhat.com>
2017-03-29 12:20:56 +0200

    Test DATADIR permissions and fail cleanly.
    Resolves: #99

--- 3.2/root/usr/share/container-scripts/mongodb/common.sh
+++ 3.2/root/usr/share/container-scripts/mongodb/common.sh
@@ -165,6 +165,18 @@ function setup_keyfile() {
   mongo_common_args+=" --keyFile ${MONGODB_KEYFILE_PATH}"
 }
 
+# setup_default_datadir checks permissions of mounded directory into default
+# data directory MONGODB_DATADIR
+function setup_default_datadir() {
+  if [ ! -w "$MONGODB_DATADIR" ]; then
+    echo >&2 "ERROR: Couldn't write into ${MONGODB_DATADIR}"
+    echo >&2 "CAUSE: current user doesn't have permissions for writing to ${MONGODB_DATADIR} directory"
+    echo >&2 "DETAILS: current user id = $(id -u), user groups: $(id -G)"
+    echo >&2 "DETAILS: directory permissions: $(stat -c '%A owned by %u:%g, SELinux: %C' "${MONGODB_DATADIR}")"
+    exit 1
+  fi
+}
+
 # info prints a message prefixed by date and time.
 function info() {
   printf "=> [%s] %s\n" "$(date +'%a %b %d %T')" "$*"
