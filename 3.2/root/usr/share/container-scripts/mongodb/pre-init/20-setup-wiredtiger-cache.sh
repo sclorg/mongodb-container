b766f6dc545d27f9aa62c053059d0e0ee157d6ea
Marek Skalický <mskalick@redhat.com>
2018-10-23 16:28:38 +0100

    Remove release label - in Fedora it's updated automatically by build system.
    Select python binary on runtime - Fedora images don't ship python
    command by default
    
    Remove arch label - 'Optional: if omitted, it will be built for
    all supported Fedora Architectures'

--- 3.2/root/usr/share/container-scripts/mongodb/pre-init/20-setup-wiredtiger-cache.sh
+++ 3.2/root/usr/share/container-scripts/mongodb/pre-init/20-setup-wiredtiger-cache.sh
@@ -1,3 +1,7 @@
+# New systems by default use only python3, so select python on runtime
+PYTHON=python3
+command -v $PYTHON &>/dev/null || PYTHON=python
+
 # setup_wiredtiger_cache checks amount of available RAM (it has to use cgroups in container)
 # and if there are any memory restrictions set storage.wiredTiger.engineConfig.cacheSizeGB
 # in MONGODB_CONFIG_PATH to upstream default size
@@ -6,12 +10,12 @@ function setup_wiredtiger_cache() {
   local config_file
   config_file=${1:-$MONGODB_CONFIG_PATH}
 
-  declare $(cgroup-limits)
+  declare $($PYTHON /usr/libexec/cgroup-limits)
   if [[ ! -v MEMORY_LIMIT_IN_BYTES || "${NO_MEMORY_LIMIT:-}" == "true" ]]; then
     return 0;
   fi
 
-  cache_size=$(python -c "min=1; limit=int(($MEMORY_LIMIT_IN_BYTES / pow(2,30) - 1) * 0.6); print( min if limit < min else limit)")
+  cache_size=$($PYTHON -c "min=1; limit=int(($MEMORY_LIMIT_IN_BYTES / pow(2,30) - 1) * 0.6); print( min if limit < min else limit)")
   echo "storage.wiredTiger.engineConfig.cacheSizeGB: ${cache_size}" >> ${config_file}
 
   info "wiredTiger cacheSizeGB set to ${cache_size}"
